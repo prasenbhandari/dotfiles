78f84707201a2fa37331e0ff066055ef1dde417f
prasenbhandari <prasenbhandari2003@gmail.com>
2025-07-12 13:02:41 +0530

    fish changes

--- fish/config.fish
+++ fish/config.fish
@@ -20,3 +20,7 @@ alias speedtest 'speedtest --secure'
 # Created by `pipx` on 2024-11-29 05:29:10
 set PATH $PATH /home/prasen/.local/bin
 set path $PATH /home/prasen/.local/share/gem/ruby/3.3.0/bin
+
+set -Ux HADOOP_HOME ~/hadoop
+set -Ux JAVA_HOME /usr/lib/jvm/java-11-openjdk
+set -x PATH $PATH $HADOOP_HOME/bin $HADOOP_HOME/sbin
